200bae5b51139ed8574f2dbfda216130a987ca19
adpachec <pachees19@gmail.com>
2023-10-31 13:23:13 +0100

    a

--- srcs/requirements/entrypoint.sh
+++ srcs/requirements/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 # Crea el directorio si no existe (esto se ejecuta en el contenedor, no en el host)
-mkdir -p /ruta/del/directorio
+mkdir -p /home/adpachec/
+mkdir -p /home/adpachec/data
+mkdir -p /home/adpachec/data/wordpress
+mkdir -p /home/adpachec/data/mariadb
 
 # Ejecuta el comando principal (por ejemplo, iniciar nginx o mariadb)
 exec "$@"
