09c3399af3f7f7cf8ac427f761d18fb95e38a1c9
root <root@LAPTOP-UTKVD2O1>
2023-11-08 13:46:59 +0100

    script

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
-# Exit immediately if a command exits with a non-zero status.
-set -e
-
-# Install the database if it's not already installed
 mysql_install_db
+if [ $? -ne 0 ]; then
+  echo "Failed to install MySQL."
+  exit 1
+fi
 
-# Check if the WordPress database directory already exists
-if [ -d "/var/lib/mysql/wp-content" ];
-then
-	echo "Database already exits"
+if [ -d "/var/lib/mysql/wp-content" ]; then
+  echo "Database already exists."
+  exit 1
 else
 
-	/etc/init.d/mysql start
-	# Start the database server
+  /etc/init.d/mysql start
+  if [ $? -ne 0 ]; then
+    echo "Failed to start MySQL."
+    exit 1
+  fi
 
-	# Secure the installation
-	mysql_secure_installation << _EOF_
-	
+  mysql_secure_installation << _EOF_
 Y
 \`${MYSQL_ROOT_PASSWORD}\`
 \`${MYSQL_ROOT_PASSWORD}\`
@@ -26,17 +26,44 @@ n
 Y
 Y
 _EOF_
-	# Give the server time to start up before running additional commands
-	sleep 3
 
-    # Create the WordPress database and user
-	mysql -e "CREATE DATABASE IF NOT EXISTS \`${WP_DB_NAME}\`;"
-	mysql -e "CREATE USER IF NOT EXISTS \`${WP_DB_USER}\`@'localhost' IDENTIFIED BY '${WP_DB_PASSWORD}';"
-	mysql -e "GRANT ALL PRIVILEGES ON \`${WP_DB_NAME}\`.* TO \`${WP_DB_USER}\`@'%' IDENTIFIED BY '${WP_DB_PASSWORD}';"
-	mysql -e "FLUSH PRIVILEGES;"
+  if [ $? -ne 0 ]; then
+    echo "Failed to secure MySQL installation."
+    /etc/init.d/mysql stop
+    exit 1
+  fi
 
-	/etc/init.d/mysql stop
+  sleep 3
 
+  mysql -e "CREATE DATABASE IF NOT EXISTS \`${WP_DB_NAME}\`;"
+  if [ $? -ne 0 ]; then
+    echo "Failed to create database."
+    /etc/init.d/mysql stop
+    exit 1
+  fi
+
+  mysql -e "CREATE USER IF NOT EXISTS \`${WP_DB_USER}\`@'localhost' IDENTIFIED BY '${WP_DB_PASSWORD}';"
+  if [ $? -ne 0 ]; then
+    echo "Failed to create user."
+    /etc/init.d/mysql stop
+    exit 1
+  fi
+
+  mysql -e "GRANT ALL PRIVILEGES ON \`${WP_DB_NAME}\`.* TO \`${WP_DB_USER}\`@'%';"
+  if [ $? -ne 0 ]; then
+    echo "Failed to grant privileges."
+    /etc/init.d/mysql stop
+    exit 1
+  fi
+
+  mysql -e "FLUSH PRIVILEGES;"
+  if [ $? -ne 0 ]; then
+    echo "Failed to flush privileges."
+    /etc/init.d/mysql stop
+    exit 1
+  fi
+
+  /etc/init.d/mysql stop
 fi
 
 exec "$@"
