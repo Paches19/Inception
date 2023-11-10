f1ce3079a4093cae06a1154c90903f69cc607f4f
root <root@LAPTOP-UTKVD2O1>
2023-11-10 12:20:02 +0100

    a

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+# Función para verificar si MariaDB está lista
+wait_for_mariadb() {
+    while ! mysqladmin ping -h"localhost" --silent; do
+        echo "Esperando a que MariaDB esté lista..."
+        sleep 1
+    done
+}
+
 # Esperar a que MariaDB esté completamente lista
-# (ajusta el tiempo de espera según sea necesario)
-sleep 10
+wait_for_mariadb
 
 # Ejecutar comandos SQL usando variables de entorno
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${WP_DB_NAME};"
@@ -10,3 +17,4 @@ mysql -u root -e "CREATE USER IF NOT EXISTS '${WP_DB_USER}'@'%' IDENTIFIED BY '$
 mysql -u root -e "GRANT ALL PRIVILEGES ON ${WP_DB_NAME}.* TO '${WP_DB_USER}'@'%';"
 mysql -u root -e "FLUSH PRIVILEGES;"
 mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+
