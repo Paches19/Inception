dcafcc19b3200731738f5a15ef6bb6a8b7127c24
root <root@LAPTOP-UTKVD2O1>
2023-11-10 11:36:34 +0100

    a

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -1,70 +1,16 @@
 #!/bin/sh
 
-mysql_install_db
-if [ $? -ne 0 ]; then
-  echo "Failed to install MySQL."
-  exit 1
-fi
+# Iniciar el servicio de MariaDB
+service mysql start
 
-if [ -d "/var/lib/mysql/wp-content" ]; then
-  echo "Database already exists."
-  exit 1
-else
+# Ejecutar el script SQL inicial
+mysql < /usr/local/bin/init_db.sql
 
-  /etc/init.d/mysql start
-  if [ $? -ne 0 ]; then
-    echo "Failed to start MySQL."
-    exit 1
-  fi
+# Limpiar después de la ejecución
+rm -f /usr/local/bin/init_db.sql
 
-  mysql_secure_installation << _EOF_
+# Detener el servicio para que CMD pueda tomar el control
+service mysql stop
 
-Y
-\`${MYSQL_ROOT_PASSWORD}\`
-\`${MYSQL_ROOT_PASSWORD}\`
-Y
-n
-Y
-Y
-_EOF_
-
-  if [ $? -ne 0 ]; then
-    echo "Failed to secure MySQL installation."
-    /etc/init.d/mysql stop
-    exit 1
-  fi
-
-  sleep 3
-
-  mysql -e "CREATE DATABASE IF NOT EXISTS \`${WP_DB_NAME}\`;"
-  if [ $? -ne 0 ]; then
-    echo "Failed to create database."
-    /etc/init.d/mysql stop
-    exit 1
-  fi
-
-  mysql -e "CREATE USER IF NOT EXISTS \`${WP_DB_USER}\`@'localhost' IDENTIFIED BY '${WP_DB_PASSWORD}';"
-  if [ $? -ne 0 ]; then
-    echo "Failed to create user."
-    /etc/init.d/mysql stop
-    exit 1
-  fi
-
-  mysql -e "GRANT ALL PRIVILEGES ON \`${WP_DB_NAME}\`.* TO \`${WP_DB_USER}\`@'%';"
-  if [ $? -ne 0 ]; then
-    echo "Failed to grant privileges."
-    /etc/init.d/mysql stop
-    exit 1
-  fi
-
-  mysql -e "FLUSH PRIVILEGES;"
-  if [ $? -ne 0 ]; then
-    echo "Failed to flush privileges."
-    /etc/init.d/mysql stop
-    exit 1
-  fi
-
-  /etc/init.d/mysql stop
-fi
-
+# Ejecutar el comando pasado al contenedor (en este caso, mysqld)
 exec "$@"
