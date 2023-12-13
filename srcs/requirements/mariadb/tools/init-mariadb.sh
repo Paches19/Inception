cca51dd1ce105b41b275bcac1a85376a368ce5d9
root <root@LAPTOP-UTKVD2O1>
2023-12-13 20:31:49 +0100

    wp config

--- srcs/requirements/mariadb/tools/init-mariadb.sh
+++ srcs/requirements/mariadb/tools/init-mariadb.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-chown -R mysql: /var/lib/mysql
-chmod 777 /var/lib/mysql
+service mysql start
 
-mysql_install_db >/dev/null 2>&1
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE ;" > db1.sql
+echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" >> db1.sql
+echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' ;" >> db1.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' ;" >> db1.sql
+echo "FLUSH PRIVILEGES;" >> db1.sql
+mysql < db1.sql
 
-if [ ! -d "/var/lib/mysql/$DB_DATABASE" ]; then
-	rm -f "$MYSQL_INIT_FILE"
-	echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE ;" > "$MYSQL_INIT_FILE"
-	echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" >> "$MYSQL_INIT_FILE"
-	echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' ;" >> "$MYSQL_INIT_FILE"
-	echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' ;" >> "$MYSQL_INIT_FILE"
-	echo "FLUSH PRIVILEGES;" >> "$MYSQL_INIT_FILE"
-	mysqld_safe --init-file=$MYSQL_INIT_FILE >/dev/null 2>&1
-else
-	mysqld_safe >/dev/null 2>&1
-fi
+kill $(cat /var/run/mysqld/mysqld.pid)
+
+mysqld
