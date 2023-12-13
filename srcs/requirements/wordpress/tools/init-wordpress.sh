98fcdde892354c3031902c4bc117b0f3fc360ac8
root <root@LAPTOP-UTKVD2O1>
2023-12-13 17:07:29 +0100

    wp corr

--- srcs/requirements/wordpress/tools/init-wordpress.sh
+++ srcs/requirements/wordpress/tools/init-wordpress.sh
@@ -8,14 +8,6 @@ if [ -f  -f /var/www/html/wordpress/wp-config.php ]
 then
     echo "Wordpress already installed, skipping download and configuration."
 else
-    # wp core download --allow-root;
-    # wp config create --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOSTNAME --allow-root;
-    # wp core install --allow-root --url=https://adpachec.42.fr --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWD --admin_email=$WP_ADMIN_MAIL --allow-root;
-    # wp user create $WP_USER $WP_USER_MAIL --role=author --user_pass=$WP_USER_PASSWD --allow-root
-    # wp option update comment_previously_approved 0 --allow-root;
-    # wp option update comment_notify 0 --allow-root;
-    # wp option update moderation_notify 0 --allow-root;
-    # wp theme install escutcheon --activate --allow-root
    curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar \
         && chmod +x wp-cli.phar \
         && mv wp-cli.phar /usr/local/bin/wp
@@ -35,4 +27,4 @@ else
 
 fi
 
-/usr/sbin/php-fpm7.3 --nodaemonize
+/usr/sbin/php-fpm7.4 --nodaemonize
