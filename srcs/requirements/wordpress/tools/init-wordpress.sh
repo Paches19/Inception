785e4dd093905571b377664ab4bc7090e7776cb1
root <root@LAPTOP-UTKVD2O1>
2023-12-13 17:13:20 +0100

    wp corr

--- srcs/requirements/wordpress/tools/init-wordpress.sh
+++ srcs/requirements/wordpress/tools/init-wordpress.sh
@@ -17,8 +17,8 @@ else
 	cd /var/www/html/wordpress;
 
 	wp core download --allow-root;
-	wp config create --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOSTNAME --allow-root;
-	wp core install --allow-root --url=https://adpachec.42.fr --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWD --admin_email=$WP_ADMIN_MAIL --allow-root;
+	wp config create --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb:3306 --allow-root;
+	wp core install --allow-root --url=adpachec.42.fr --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWD --admin_email=$WP_ADMIN_MAIL --allow-root;
 	wp user create $WP_USER $WP_USER_MAIL --role=author --user_pass=$WP_USER_PASSWD --allow-root;
 
 	wp option update comment_previously_approved 0 --allow-root;
