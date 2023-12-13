40a7b240bba3ea877ac27b34d570a7206f08923c
root <root@LAPTOP-UTKVD2O1>
2023-12-13 12:23:24 +0100

    comentarios

--- srcs/requirements/wordpress/tools/init-wordpress.sh
+++ srcs/requirements/wordpress/tools/init-wordpress.sh
@@ -8,6 +8,9 @@ else
     && wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOSTNAME --allow-root \
     && wp core install --url=https://adpachec.42.fr --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWD --admin_email=$WP_ADMIN_MAIL --allow-root \
     && wp user create $WP_USER $WP_USER_MAIL --role=author --user_pass=$WP_USER_PASSWD --porcelain --allow-root
+    wp option update comment_previously_approved 0 --allow-root;
+    wp option update comment_notify 0 --allow-root;
+    wp option update moderation_notify 0 --allow-root;
 fi
 
 /usr/sbin/php-fpm7.3 -F -R
