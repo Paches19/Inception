8415d36eff80ad2e48adc5fc50ef0e3229d8f8e2
root <root@LAPTOP-UTKVD2O1>
2023-12-13 14:01:39 +0100

    bullseye

--- srcs/requirements/wordpress/tools/init-wordpress.sh
+++ srcs/requirements/wordpress/tools/init-wordpress.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-if [ -f ./wp-config.php ]
+sed -i "s/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/" "/etc/php/7.4/fpm/pool.d/www.conf";
+mkdir -p /run/php/;
+touch /run/php/php7.4-fpm.pid;
+
+if [ -f  -f /var/www/html/wordpress/wp-config.php ]
 then
     echo "Wordpress already installed, skipping download and configuration."
 else
@@ -16,18 +20,13 @@ else
         && chmod +x wp-cli.phar \
         && mv wp-cli.phar /usr/local/bin/wp
 
-	chmod -R 755 /var/www/html;
-	mkdir -p /var/www/html \
-        && chown -R www-data:www-data /var/www/html
-
-	cd /var/www/html;
+	chmod -R 775 /var/www/html/wordpress;
+	chown -R www-data:www-data /var/www/html/wordpress;
+	cd /var/www/html/wordpress;
 
 	wp core download --allow-root;
-
 	wp config create --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOSTNAME --allow-root;
-
 	wp core install --allow-root --url=https://adpachec.42.fr --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWD --admin_email=$WP_ADMIN_MAIL --allow-root;
-
 	wp user create $WP_USER $WP_USER_MAIL --role=author --user_pass=$WP_USER_PASSWD --allow-root;
 
 	wp option update comment_previously_approved 0 --allow-root;
